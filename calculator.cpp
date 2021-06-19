84e3feb7db24a14ec71199751f2597b400893f05
hayelom <hayelomkiros20@gmail.com>
2021-06-19 13:51:56 +0300

    setting up

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+float add(float x, float y){
+    return x + y;
+}
+float number1,number2;
+int main(){
+    float result = 0;
+    int choice;
+    cout << "calculator\n";
+    cout << "\nmenu\n";
+    cout<< "\n 1.addition";
+    cout<< "\n 2.multiplication";
+    cout<< "\n 1.division";
+    cout<< "\n 1.substraction";
+    cin >>choice;
+    cout<< "\nEnter the first number";
+    cin>>number1;
+    cout<< "\nEnter the second number";
+    cin>>number2;
+    switch(choice){
+        case 1: //addition
+         break;
+         
+    }
+}
